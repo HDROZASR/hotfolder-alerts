f491945df6a4902d6268a2047b761cae63425265
RobertEpam <Robert_Rozas@epam.com>
2024-05-22 14:37:53 -0400

    all tested and updated

--- hotFolderAlertsDaily.groovy
+++ hotFolderAlertsDaily.groovy
@@ -13,6 +13,7 @@ dateNow = LocalDateTime.now()
 
 Calendar myDate = Calendar.getInstance() 
 dow = myDate.get (Calendar.DAY_OF_WEEK)
+flag = 0
 
 
 def runQuery(String queryString, String fileName){
@@ -26,28 +27,35 @@ def runQuery(String queryString, String fileName){
     query = new FlexibleSearchQuery(queryWeekly)
     query.setResultClassList(Arrays.asList(Integer.class, String.class, String.class))
     searchResult = flexibleSearchService.search(query)
-    message = '...Check for missing file list for ' + fileName +'-**.csv... '
+    message = '...Check for missing file list for ' + fileName +'-**.csv... STARTED ...'
     println(message) 
     emailBody.append(message).append(System.lineSeparator())
 
     lsize = searchResult.getResult().size()
     if(  lsize == 0 ){
-        message = '...0 files found...'
+        message = '...0 files found...MISSING FILE'
+        flag = 1
         println(message)
     }
     else{  
         myFiles = new StringBuilder()   
         for (item in searchResult.getResult()) {
-            myFiles.append(item[1]).append(' ').append(item[2]).append(System.lineSeparator())           
+            myFiles.append(item[1]).append(' ').append(item[2]).append(System.lineSeparator())   
+            if(item[2] == 'FAILURE'){
+                flag = 1
+            }        
         }
-        message = '...' + myFiles.toString()
+        message = myFiles.toString()
         println(message)   
     }
 
-    emailBody.append(message).append(System.lineSeparator())
-    message = '...Check for missing file list for ' + fileName +'-**.csv.. FINISHED... '
+    emailBody.append(message)
+    if(  lsize == 0 ){
+        emailBody.append(System.lineSeparator()) 
+    }
+    message = '...Check for missing file list for ' + fileName +'-**.csv.. FINISHED ... '
     println(message) 
-    emailBody.append(message).append(System.lineSeparator())  
+    emailBody.append(message).append(System.lineSeparator()).append(System.lineSeparator())  
 }
 
  if(dow == Calendar.MONDAY){
@@ -73,41 +81,46 @@ def runQuery(String queryString, String fileName){
 
 emailBody.append(message).append(System.lineSeparator())
 // This file arrives to Hot-Folder daily material_extract_hd<**>store-*.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'material_extract%'",'material_extract')    
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'material_extract%'",'material_extract')    
 // This file arrives to Hot-Folder daily dealer_master_extract_hd<**>-*.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'dealer_master_extract_hd%'",'dealer_master_extract_hd')  
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'dealer_master_extract_hd%'",'dealer_master_extract_hd')  
 // This file arrives to Hot-Folder daily bv_harleydavidson_ratings-*.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'bv_harleydavidson_ratings%'",'bv_harleydavidson_ratings') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'bv_harleydavidson_ratings%'",'bv_harleydavidson_ratings') 
 // This file arrives to Hot-Folder daily bv_harleydavidson_emea_ratings-*.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'bv_harleydavidson_emea_ratings%'",'bv_harleydavidson_emea_ratings') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'bv_harleydavidson_emea_ratings%'",'bv_harleydavidson_emea_ratings') 
 // This file arrives to Hot-Folder daily plm_parts_extract_hdus_delta-*.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'plm_parts_extract%'",'plm_parts_extract') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'plm_parts_extract%'",'plm_parts_extract') 
 // This file arrives to Hot-Folder daily inventory_master_extract-**.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'inventory_master_extract%'",'inventory_master_extract') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'inventory_master_extract%'",'inventory_master_extract') 
 // This file arrives to Hot-Folder daily dealer_inventory_delta-**.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'dealer_inventory_delta%'",'dealer_inventory_delta') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'dealer_inventory_delta%'",'dealer_inventory_delta') 
 // This file arrives to Hot-Folder daily dropship_inventory-**.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'dropship_inventory%'",'dropship_inventory') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'dropship_inventory%'",'dropship_inventory') 
 // This file arrives to Hot-Folder daily fitment_extract_delta-**.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'fitment_extract_delta%'",'fitment_extract_delta') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'fitment_extract_delta%'",'fitment_extract_delta') 
 // This file arrives to Hot-Folder daily commerce_removed_assets_manifest-**.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'commerce_removed_assets_manifest%'",'commerce_removed_assets_manifest') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'commerce_removed_assets_manifest%'",'commerce_removed_assets_manifest') 
 // This file arrives to Hot-Folder daily commerce_assets_manifest-**.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'commerce_assets_manifest%'",'commerce_assets_manifest') 
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'commerce_assets_manifest%'",'commerce_assets_manifest') 
 // This file arrives to Hot-Folder daily isheet_extract_delta-**.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'isheet_extract_delta%'",'isheet_extract_delta')    
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'isheet_extract_delta%'",'isheet_extract_delta')    
 // This file arrives to Hot-Folder daily order_schedline_extract-**.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'order_schedline_extract%'",'order_schedline_extract')    
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'order_schedline_extract%'",'order_schedline_extract')    
 // This file arrives to Hot-Folder daily material_extract_hd<**>store-*.csv
-runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'material_extract%'",'material_extract')   
-
+runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'material_extract%'",'material_extract')   
+//-24 and run at 7am
+// if 1 of the check for cero files is true, then change subject to or add missing files - something wrong
 
 // Create the email
 email = de.hybris.platform.util.mail.MailUtils.getPreConfiguredEmail()
 email.setFrom('AutomaticNotificator@harley-davidson.com')
 email.addTo('SupportHDD1-SUPPHybris@epam.com')
 email.addTo('supporthdd1-epm-sd-l1@harley-davidson.com')
+
 email.subject = 'Daily HotFolder files check'
+if(flag){
+  email.subject = email.subject + ' - Missing files found'
+}
 email.msg = emailBody.toString()
 // Send the email
 email.send()
--- hotFolderAlertsWeekly.groovy
+++ hotFolderAlertsWeekly.groovy
@@ -9,7 +9,7 @@ println(message)
 
 emailBody.append(message).append(System.lineSeparator());
 dateNow = LocalDateTime.now()
-
+flag = 0
 
 Calendar myDate = Calendar.getInstance() 
 dow = myDate.get (Calendar.DAY_OF_WEEK)
@@ -26,61 +26,64 @@ def runQuery(String queryString, String fileName){
     query = new FlexibleSearchQuery(queryWeekly)
     query.setResultClassList(Arrays.asList(Integer.class, String.class, String.class))
     searchResult = flexibleSearchService.search(query)
-    message = '...Check for missing file list for ' + fileName +'-**.csv... '
+    message = '...Check for missing file list for ' + fileName +'-**.csv... STARTED ...'
     println(message) 
     emailBody.append(message).append(System.lineSeparator())
 
     lsize = searchResult.getResult().size()
     if(  lsize == 0 ){
-        message = '...0 files found...'
+        message = '...0 files found...MISSING FILE'
+        flag = 1
         println(message)
     }
     else{  
         myFiles = new StringBuilder()   
         for (item in searchResult.getResult()) {
-            myFiles.append(item[1]).append(' ').append(item[2]).append(System.lineSeparator())           
+            myFiles.append(item[1]).append(' ').append(item[2]).append(System.lineSeparator())  
+            if(item[2] == 'FAILURE'){
+                flag = 1
+            }         
         }
-        message = '...' + myFiles.toString()
+        message = myFiles.toString()
         println(message)   
     }
 
-    emailBody.append(message).append(System.lineSeparator())
-    message = '...Check for missing file list for ' + fileName +'-**.csv.. FINISHED... '
+    emailBody.append(message)
+    if(  lsize == 0 ){
+        emailBody.append(System.lineSeparator()) 
+    }
+    message = '...Check for missing file list for ' + fileName +'-**.csv.. FINISHED ... '
     println(message) 
-    emailBody.append(message).append(System.lineSeparator())  
+    emailBody.append(message).append(System.lineSeparator()).append(System.lineSeparator())  
 }
 
 if(dow == Calendar.SATURDAY){
     // This file arrives to Hot-Folder at Saturday plm_parts_extract_hdus_full-*.csv  
-    runQuery("select {pk},{mh.key},{ms.code} from { MonitorHistoryData as mh left join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'plm_parts_extract_hdus_full%'",'plm_parts_extract_hdus_full')
+    runQuery("select {pk},{mh.key},{ms.code} from { MonitorHistoryData as mh left join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'plm_parts_extract_hdus_full%'",'plm_parts_extract_hdus_full')
     
     // This file arrives to Hot-Folder at Saturday dealer_inventory_full-**.csv  
-    runQuery("select {pk},{mh.key},{ms.code} from { MonitorHistoryData as mh left join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'dealer_inventory_full%'",'dealer_inventory_full')
+    runQuery("select {pk},{mh.key},{ms.code} from { MonitorHistoryData as mh left join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'dealer_inventory_full%'",'dealer_inventory_full')
     
     // This file arrives to Hot-Folder at Saturday fitment_extract_full-**.csv  
-    runQuery("select {pk},{mh.key},{ms.code} from { MonitorHistoryData as mh left join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'fitment_extract_full%'",'fitment_extract_full')       
+    runQuery("select {pk},{mh.key},{ms.code} from { MonitorHistoryData as mh left join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'fitment_extract_full%'",'fitment_extract_full')       
 
     // This file arrives to Hot-Folder at Saturday isheet_extract_full-**.csv  
-    runQuery("select {pk},{mh.key},{ms.code} from { MonitorHistoryData as mh left join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'isheet_extract_full%'",'isheet_extract_full')       
+    runQuery("select {pk},{mh.key},{ms.code} from { MonitorHistoryData as mh left join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'isheet_extract_full%'",'isheet_extract_full')       
 }
 
-if(dow == Calendar.MONDAY || dow == Calendar.TUESDAY || dow == Calendar.WEDNESDAY){    
+if(dow == Calendar.MONDAY ||  dow == Calendar.WEDNESDAY){    
     if(dow == Calendar.MONDAY){
         message = '...Monday checking for CA file...'
         println(message)
-    }
-    if(dow == Calendar.TUESDAY){
-        message = '...Tuesday checking for EMEA file...'
-        println(message)
-    }
+    }    
     if(dow == Calendar.WEDNESDAY){
         message = '...Wednesday checking for US file...'
         println(message)
     }
     emailBody.append(message).append(System.lineSeparator())
-    runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -20, current_timestamp) and {mh.key} like 'pricing_master_extract%'",'pricing_master_extract')    
+    runQuery("select {PK},{mh.key},{ms.code} from { MonitorHistoryData as mh join MonitorStatus as ms on {mh.status} = {ms.pk} } where {mh.creationTime} > DATEADD(hour, -24, current_timestamp) and {mh.key} like 'pricing_master_extract%'",'pricing_master_extract')    
 }
-
+//-24 and 11:59pm
 
 // Create the email
 email = de.hybris.platform.util.mail.MailUtils.getPreConfiguredEmail()
@@ -88,6 +91,9 @@ email.setFrom('AutomaticNotificator@harley-davidson.com')
 email.addTo('SupportHDD1-SUPPHybris@epam.com')
 email.addTo('supporthdd1-epm-sd-l1@harley-davidson.com')
 email.subject = 'Weekly HotFolder files check'
+if(flag){
+  email.subject = email.subject + ' - Missing files found'
+}
 email.msg = emailBody.toString()
 // Send the email
 email.send()
